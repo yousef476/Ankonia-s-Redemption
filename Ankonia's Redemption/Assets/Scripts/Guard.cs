c89c402522147dbee36288ae7a08d659810362d9
AyaHesham8 <ayaaheshaam73@gmail.com>
2021-12-29 18:34:24 +0200

    ..

--- Ankonia's Redemption/Assets/Scripts/Guard.cs
+++ Ankonia's Redemption/Assets/Scripts/Guard.cs
@@ -5,61 +5,113 @@ using UnityEngine;
 public class Guard : MonoBehaviour
 {
     // Start is called before the first frame update
+    private float dirX;
+    public float guardSpeed;
+    private Rigidbody2D rb;
+    private bool isFacingRight = false;
+    private Vector3 localScale;
+    public int guardHealth;
+    public int guardDamage;
     public GameObject player;
     private Transform playerPos;
     private Vector2 currentPos;
     public float distance;
-    public float enemySpeed;
-    public bool isFacingRight;
-    public int damage;
+    private Animator anim;
     void Start()
     {
+        localScale = transform.localScale;
+        rb = GetComponent<Rigidbody2D>();
+        dirX = -1f;
         playerPos = player.GetComponent<Transform>();
         currentPos = GetComponent<Transform>().position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Vector2.Distance(transform.position, playerPos.position) < distance)
-        {
-
-            //Flip();
-            transform.position = Vector2.MoveTowards(transform.position, playerPos.position, enemySpeed * Time.deltaTime);
-            Attack();
-        }
-    }
-    private void FixedUpdate()
-    {
-        if (this.isFacingRight == true)
-        {
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(enemySpeed, this.GetComponent<Rigidbody2D>().velocity.y);
-        }
-        else
-        {
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(-enemySpeed, this.GetComponent<Rigidbody2D>().velocity.y);
-        }
-    }
-    public void Flip()
-    {
-        isFacingRight = !isFacingRight;
-        transform.localScale = new Vector3(-(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-
+        anim = GetComponent<Animator>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Turn" || collision.tag == "Enemy")
+        if (collision.tag == "GROUND" )
         {
-            Flip();
+            anim.SetBool("Stuck", true);
+
         }
-        else if (collision.tag == "Player")
+        else if(collision.tag == "Player")
         {
-            Attack();
+           // Attack();
+        }
+        else if(collision.tag == "Enemy")
+        {
+            dirX *= -1f;
+
         }
 
     }
-    void Attack()
+    private void Update()
     {
+        anim.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+        anim.SetBool("Near", false);
+
+        Debug.Log("Distance : " + distance.ToString());
+        if (Vector2.Distance(transform.position, playerPos.position) < distance)
+        {
+            Debug.Log("If: " + Vector2.Distance(transform.position, playerPos.position).ToString());
+            anim.SetBool("Near", true);
+
+
+            //Flip();
+            transform.position = Vector2.MoveTowards(transform.position, playerPos.position, guardSpeed * Time.fixedDeltaTime);
+            CheckWhereToFace();
+            //Attack();
+        }
+
+        
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX * guardSpeed, rb.velocity.y);
+    }
+
+
+    private void LateUpdate()
+    {
+        CheckWhereToFace();
+    }
+    void CheckWhereToFace()
+    {
+        if (dirX > 0)
+            isFacingRight = true;
+        else if (dirX < 0)
+            isFacingRight = false;
+
+        if (((isFacingRight) && (localScale.x < 0)) || ((!isFacingRight) && (localScale.x > 0)))
+            localScale.x *= -1;
+
+
+        transform.localScale = localScale;
 
     }
-}
+
+
+    //void Attack()
+    //{
+    //    anim.SetBool("Near", true);
+    //    if (guardHealth > 0 && FindObjectOfType<PlayerStats>().health > 0 )
+    //    {
+    //        FindObjectOfType<PlayerStats>().TakeDamage(guardDamage);
+    //    }
+    //    else if (guardHealth <= 0 && FindObjectOfType<PlayerStats>().health > 0)
+    //    {
+    //        GuardDie();
+    //    }
+    //    else if(guardHealth > 0 && FindObjectOfType<PlayerStats>().health <= 0)
+    //    {
+    //        FindObjectOfType<PlayerStats>().Die();
+
+    //    }
+
+    //}
+        //void GuardDie()
+        //{
+        //    Debug.Log("Guard died");
+        //    anim.SetBool("isDead", true);
+           
+        //}
+    }
